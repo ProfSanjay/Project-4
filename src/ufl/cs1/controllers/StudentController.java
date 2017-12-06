6693cf7ba982eda0693d56f172e5f3d357402046
ProfSanjay <nsamlal@ufl.edu>
2017-12-05 22:26:59 -0500

    Power Pill Defender Test 1 Game freezes after 13 seconds due to an ArrayOutofBounds Exception. The ghosts also seem to try and protect power pills thar are have been eaten.

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -2,8 +2,10 @@
 package ufl.cs1.controllers;
 
 import game.controllers.DefenderController;
+import game.models.Attacker;
 import game.models.Defender;
 import game.models.Game;
+import game.models.Node;
 
 import java.util.List;
 
@@ -23,11 +25,37 @@ public final class StudentController implements DefenderController
 		for(int i = 0; i < actions.length; i++)
 		{
 			Defender defender = enemies.get(i);
-			List<Integer> possibleDirs = defender.getPossibleDirs();
+			/*List<Integer> possibleDirs = defender.getPossibleDirs();
 			if (possibleDirs.size() != 0)
 				actions[i]=possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
 			else
-				actions[i] = -1;
+				actions[i] = -1;*/
+
+			List<Node> powerPills = game.getPowerPillList();
+			Attacker player = game.getAttacker();
+
+			int countSize = powerPills.size(), powerPillContact = 0;
+			int counter = 0;
+
+			if (powerPills.get(counter) == null)
+				counter++;
+			if (defender.getLocation().getPathDistance(powerPills.get(counter)) < 50) {
+				counter++;
+
+				if (powerPills.get(counter) == null && powerPills.get(counter) == defender.getLocation() )
+					powerPillContact++;
+
+				if (powerPillContact == 3)
+					powerPillContact = 0;
+
+				break;
+			}
+			if(counter == countSize)
+				counter = 0;
+
+			actions[i] = defender.getNextDir(powerPills.get(counter), true);
+
+
 		}
 		return actions;
 	}

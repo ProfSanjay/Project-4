46e75b4db816568022ffdaac7e34d24bc57abf0e
ebejleri <eric.bejleri@ufl.edu>
2017-12-10 12:50:03 -0500

    OK everyone code pls blessrng

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -7,65 +7,89 @@ import game.models.Node;
 
 import java.util.List;
 
-public final class StudentController implements DefenderController
-{
+public final class StudentController implements DefenderController {
 	public void init(Game game) {
 		//ohohohohoho
 	}
 
-	public void shutdown(Game game) { }
+	public void shutdown(Game game) {
+	}
 
-	public int[] update(Game game,long timeDue)
-	{
+	private void chargeStrategy(int[] actions, int i, Defender defender, Attacker attacker, List<Integer> possibleDirs) {
+		// If the defender can narrow the gap between him and the attacker, move towards her across the farthest axis first.
+		if ((Math.abs(attacker.getLocation().getX() - defender.getLocation().getX()) < Math.abs(attacker.getLocation().getY() - defender.getLocation().getY()))
+				&& (((possibleDirs.indexOf(Game.Direction.UP)) != -1) || (possibleDirs.indexOf(Game.Direction.DOWN)) != -1)) {
+			// When the vertical distance between the defender and the attacker is greater than the horizontal distance, move vertically.
+			if (attacker.getLocation().getY() < defender.getLocation().getY() && possibleDirs.indexOf(Game.Direction.UP) != -1) {
+				// Get attacker Y location.  If it is less than (higher up) than defender AND I can move up, then move up.
+				actions[i] = Game.Direction.UP;
+				return;
+			}
+			if (attacker.getLocation().getY() > defender.getLocation().getY() && possibleDirs.indexOf(Game.Direction.DOWN) != -1) {
+				// Get attacker Y location.  If it is greater than (lower down) defender AND I can move down, then move down.
+				actions[i] = Game.Direction.DOWN;
+				return;
+			}
+		} else {
+			if (attacker.getLocation().getX() < defender.getLocation().getX() && possibleDirs.indexOf(Game.Direction.LEFT) != -1) {
+				// Get attacker X location.  If it is less than defender AND I can move to the left, then move to the left.
+				actions[i] = Game.Direction.LEFT;
+				return;
+			}
+			if (attacker.getLocation().getX() > defender.getLocation().getX() && possibleDirs.indexOf(Game.Direction.RIGHT) != -1) {
+				// Get attacker X location.  If it is greater than defender AND I can move to the right, then move right.
+				actions[i] = Game.Direction.RIGHT;
+				return;
+			}
+		}
+		// If ideal direction is not available, pick the next best direction.
+		if (attacker.getLocation().getX() < defender.getLocation().getX() && possibleDirs.indexOf(Game.Direction.LEFT) != -1) {
+			// Get attacker X location.  If it is less than defender AND I can move to the left, then move to the left.
+			actions[i] = Game.Direction.LEFT;
+		} else if (attacker.getLocation().getX() > defender.getLocation().getX() && possibleDirs.indexOf(Game.Direction.RIGHT) != -1) {
+			// Get attacker X location.  If it is greater than defender AND I can move to the right, then move to the right.
+			actions[i] = Game.Direction.RIGHT;
+		} else if (attacker.getLocation().getY() < defender.getLocation().getY() && possibleDirs.indexOf(Game.Direction.UP) != -1) {
+			// Get attacker Y location.  If it is less than (higher up) than defender AND I can move up, then move up.
+			actions[i] = Game.Direction.UP;
+		} else if (attacker.getLocation().getY() > defender.getLocation().getY() && possibleDirs.indexOf(Game.Direction.DOWN) != -1) {
+			// Get attacker Y location.  If it is greater than (lower down) defender AND I can move down, then move down.
+			actions[i] = Game.Direction.DOWN;
+		}
+	}
+
+	public int[] update(Game game, long timeDue) {
 		int[] actions = new int[Game.NUM_DEFENDER];
 		List<Defender> enemies = game.getDefenders();
 
 		//Starts the loop for each ghost action
-		for(int i = 0; i < actions.length; i++) {
+		for (int i = 0; i < actions.length; i++) {
 			Defender defender = enemies.get(i);
 			List<Integer> possibleDirs = defender.getPossibleDirs();
-			if (i == 4) { //This is Madelyn's defender.
+			if (i == 4) { //Madelyn's Code
 				Attacker attacker = game.getAttacker();
 
-				if (attacker.getLocation().getX() < defender.getLocation().getX() && possibleDirs.indexOf(Game.Direction.LEFT) != -1) {
-					// Get attacker X location.  If it is less than defender AND I can move to the left, then move to the left.
-					actions[i] = Game.Direction.LEFT;
+					chargeStrategy(actions, i, defender, attacker, possibleDirs);
+
+					if (defender.isVulnerable()) {
+
+						if (actions[i] == Game.Direction.UP) {
+							actions[i] = Game.Direction.DOWN;
+						} else if (actions[i] == Game.Direction.DOWN) {
+							actions[i] = Game.Direction.UP;
+						} else if (actions[i] == Game.Direction.LEFT) {
+							actions[i] = Game.Direction.RIGHT;
+						} else if (actions[i] == Game.Direction.RIGHT) {
+							actions[i] = Game.Direction.LEFT;
+						}
+					} /*else {
+					if (possibleDirs.size() != 0)
+						actions[i] = possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
+					else
+						actions[i] = -1;
+				}*/
 				}
-				else if (attacker.getLocation().getX() > defender.getLocation().getX() && possibleDirs.indexOf(Game.Direction.RIGHT) != -1) {
-					// Get attacker X location.  If it is greater than defender AND I can move to the right, then move to the right.
-					actions[i] = Game.Direction.RIGHT;
-				}
-				else if (attacker.getLocation().getY() < defender.getLocation().getY() && possibleDirs.indexOf(Game.Direction.UP) != -1) {
-					// Get attacker Y location.  If it is less than (higher up) than defender AND I can move up, then move up.
-					actions[i] = Game.Direction.UP;
-				}
-				else if (attacker.getLocation().getY() > defender.getLocation().getY() && possibleDirs.indexOf(Game.Direction.DOWN) != -1) {
-					// Get attacker Y location.  If it is greater than (lower down) defender AND I can move down, then move down.
-					actions[i] = Game.Direction.DOWN;
-				}
 
-
-
-
-
-
-				//In Vulnerable Mode, make defender go in opposite direction ghost is going.
-				if (defender.isVulnerable()) {
-					if (actions[i] == Game.Direction.LEFT) {
-						actions[i] = Game.Direction.RIGHT;
-					}
-					else if (actions[i] == Game.Direction.RIGHT) {
-						actions[i] = Game.Direction.LEFT;
-					}
-					else if (actions[i] == Game.Direction.UP) {
-						actions[i] = Game.Direction.DOWN;
-					}
-					else if (actions[i] == Game.Direction.DOWN) {
-						actions[i] = Game.Direction.UP;
-					}
-				}
-			}
-			else {
 
 				if (i == 3) {//Nic's code
 					List<Node> powerPills = game.getPowerPillList();
@@ -103,7 +127,7 @@ public final class StudentController implements DefenderController
 
 					}
 				}
-			}
+
 			if(i == 2) //Preethi's code
 			{
 				List<Node> powerPills = game.getPowerPillList();

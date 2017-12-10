a26c7d07f84c5a1cca8baad8d56d74f136f72ec2
ebejleri <eric.bejleri@ufl.edu>
2017-12-10 14:15:59 -0500

    Finished Code

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -66,32 +66,28 @@ public final class StudentController implements DefenderController {
 		for (int i = 0; i < actions.length; i++) {
 			Defender defender = enemies.get(i);
 			List<Integer> possibleDirs = defender.getPossibleDirs();
-			if (i == 4) { //Madelyn's Code
+			if (i == 4) { //Madelyn Kloske's Code (Ghost 4 - Inky)
 				Attacker attacker = game.getAttacker();
 
 					chargeStrategy(actions, i, defender, attacker, possibleDirs);
 
 					if (defender.isVulnerable()) {
 
-						if (actions[i] == Game.Direction.UP) {
+						if (actions[i] == Game.Direction.UP)
 							actions[i] = Game.Direction.DOWN;
-						} else if (actions[i] == Game.Direction.DOWN) {
+
+						 else if (actions[i] == Game.Direction.DOWN)
 							actions[i] = Game.Direction.UP;
-						} else if (actions[i] == Game.Direction.LEFT) {
+
+						 else if (actions[i] == Game.Direction.LEFT)
 							actions[i] = Game.Direction.RIGHT;
-						} else if (actions[i] == Game.Direction.RIGHT) {
+
+						 else if (actions[i] == Game.Direction.RIGHT)
 							actions[i] = Game.Direction.LEFT;
 						}
-					} /*else {
-					if (possibleDirs.size() != 0)
-						actions[i] = possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
-					else
-						actions[i] = -1;
-				}*/
-				}
+					}
 
-
-				if (i == 3) {//Nic's code
+				if (i == 3) { // Nic Samlal's Code (Ghost 3 - Clyde)
 					List<Node> powerPills = game.getPowerPillList();
 					Attacker player = game.getAttacker();
 
@@ -99,37 +95,44 @@ public final class StudentController implements DefenderController {
 					int counter = 0;
 					Node attackPosition = player.getLocation();
 
-					if (countSize == 0) {
+					if (countSize == 0)
 						actions[i] = defender.getNextDir(attackPosition, true);
-					} else {
+					// Backup countermeasure used when all of the power pills have been eaten.
+
+					else {
 						if (counter >= powerPills.size()) {
 							countSize = powerPills.size();
 							counter++;
 						}
+						// Determines the amount of power pills on the map, storing that number into a variable.
+
 						if (defender.getLocation().getPathDistance(powerPills.get(counter)) < 50) {
 							countSize = powerPills.size();
 							counter++;
+						// If the distance between the ghost and a specific power pill
 
 							if (!(counter >= powerPills.size())) {
 								if (powerPills.get(counter) == null && powerPills.get(counter) == defender.getLocation())
 									powerPillContact++;
+							// Ensures that the ghost will protect each power pill, unless the power pill doesn't exist.
 
 								if (powerPillContact == 3)
 									powerPillContact = 0;
+								// When the ghost passes through a power pill 3 times, it moves on to the next power pill.
 
 								break;
 							}
 						}
 						if (counter >= countSize)
 							counter = 0;
+						// Resets the counter when all of the power pills have been checked.
 
 						actions[i] = defender.getNextDir(powerPills.get(counter), true);
 
 					}
 				}
 
-			if(i == 2) //Preethi's code
-			{
+			if(i == 2) { //Preethi Venkataraman's Code (Ghost 2 - Pinky)
 				List<Node> powerPills = game.getPowerPillList();
 				Attacker pacman = game.getAttacker();
 				Node attackPosition = pacman.getLocation();
@@ -146,108 +149,101 @@ public final class StudentController implements DefenderController {
 
 				int diffX = xOfD - x; //gets the x difference between defender and attacker
 				int diffY = yOfD - y; //gets the y difference between defender and attacker
-				int distance = 0; //
+				int distance = 0;
 
 				if (Math.abs(diffX) > Math.abs(diffY)) {
 					for (int j = 0; j < powerPills.size(); j++) {
 						powerPills = game.getPowerPillList();
 
 						if (defender.getLocation().getPathDistance(powerPills.get(j)) > distance && powerPills.get(j) != null)
-						{
 							Math.abs(diffX); //gets the difference of x-axis
-
-						}
 					}
-				} else {
+				}
+
+				else {
 					for (int j = 0; j < powerPills.size(); j++) {
 						powerPills = game.getPowerPillList();
 
-						if (defender.getLocation().getPathDistance(powerPills.get(j)) > distance && powerPills.get(j) != null) {
+						if (defender.getLocation().getPathDistance(powerPills.get(j)) > distance && powerPills.get(j) != null)
 							Math.abs(diffY);
 							//gets the difference of y-axis
-
-
-						}
 					}
 					actions[i] = defender.getNextDir(attackPosition, false);
 				}
-
 
 				if (defender.isVulnerable() == true) {
 					if (Math.abs(x - xOfD) > Math.abs(y - yOfD)) {
 						for (int j = 0; j < powerPills.size(); j++) {
 							powerPills = game.getPowerPillList();
 
-							if (defender.getLocation().getPathDistance(powerPills.get(j)) > distance && powerPills.get(j) != null) {
+							if (defender.getLocation().getPathDistance(powerPills.get(j)) > distance && powerPills.get(j) != null)
 								Math.abs(diffY);
 								//gets the difference of y-axis
-
-							}
 						}
 
 						actions[i] = defender.getNextDir(attackPosition, true);
-					} else {
+					}
+
+					else {
 						for (int j = 0; j < powerPills.size(); j++) {
 							powerPills = game.getPowerPillList();
 
-							if (defender.getLocation().getPathDistance(powerPills.get(j)) > distance && powerPills.get(j) != null) {
+							if (defender.getLocation().getPathDistance(powerPills.get(j)) > distance && powerPills.get(j) != null)
 								Math.abs(diffX);
 								//get the difference of x-axis
-
-
-							}
 						}
-
 					}
 					actions[i] = defender.getNextDir(attackPosition, false);
-
 				}
-
-
-
-
 			}
 
-			if (i == 1) {
-				//Sets up the intial data that I'll need
+			if (i == 1) { // Eric Bejreli's Code (Ghost 1 - Blinky)
+				//Sets up the initial data that I'll need
 				List<Node> powerPills = game.getPowerPillList();
 				Attacker pacman = game.getAttacker();
 				Node attackPosition = pacman.getLocation();
-				if (powerPills.size() == 0) //Will go right for the attacker if there are no powerpills
-				{
+
+				if (powerPills.size() == 0) { //Will go right for the attacker if there are no power pills
 					if (defender.isVulnerable() == true)
 						actions[i] = defender.getNextDir(attackPosition, false);
 					else actions[i] = (defender.getNextDir(attackPosition, true));
+				}
 
-				} else
+				else
 					//Checks and sees if its in a vulnerable state
+
 					if (defender.isVulnerable() == true) {
 						powerPills = game.getPowerPillList();
 						int distance = 0;
 						Node nearestPill = null;
+
 						for (int j = 0; j < powerPills.size(); j++) {
 							powerPills = game.getPowerPillList();
-							//This iterate through the powerpill list since the command for that in this project takes up too much memory
+							//This iterate through the power pill list since the command for that in this project takes up too much memory
+
 							if (defender.getLocation().getPathDistance(powerPills.get(j)) > distance && powerPills.get(j) != null) {
 								distance = defender.getLocation().getPathDistance(powerPills.get(j));
 								nearestPill = powerPills.get(j);
-
 							}
 						}
-						//If its vulnerable, it'll go towards the next powerpill location to ambush the attacker
+
+						//If its vulnerable, it'll go towards the next power pill location to ambush the attacker
 						if (nearestPill != null)
 							actions[i] = defender.getNextDir(nearestPill, true);
-						else {
-							//otherwise it will just attack the man straight up
+
+						else
 							actions[i] = defender.getNextDir(attackPosition, true);
-						}
-					} else {
-						//This is the main code if there are powerpills
+						//otherwise it will just attack the man straight up
+
+					}
+
+					else {
+						//This is the main code if there are power pills
 						powerPills = game.getPowerPillList();
 						Node nearestPill = null;
 						int distance = 100000;
 						for (int j = 0; j < powerPills.size(); j++) {
-							//This is the same code as above, it will check for nearest powerpill
+							//This is the same code as above, it will check for nearest power pill
 							powerPills = game.getPowerPillList();
 
 							if (defender.getLocation().getPathDistance(powerPills.get(j)) < distance) {
@@ -255,15 +251,16 @@ public final class StudentController implements DefenderController {
 								nearestPill = powerPills.get(j);
 							}
 						}
-						//Since I tested more complex defensive code and that wound up proving pretty ineffective
-						//this time around, I am seeing if the attacker is near the powerpill I'm defending and if so, then they will go to it in order to cut her off.
-						if (defender.getLocation().getPathDistance(nearestPill) < pacman.getLocation().getPathDistance(nearestPill)) {
-							//if she isn't close, then the attacker will go right for her. Kind of like a guard dog.
-							actions[i] = defender.getNextDir(attackPosition, true);
-						} else {
-							actions[i] = defender.getNextDir(nearestPill, true);
-						}
 
+						//Since I tested more complex defensive code and that wound up proving pretty ineffective
+						//this time around, I am seeing if the attacker is near the power pill I'm defending and if so, then they will go to it in order to cut her off.
+
+						if (defender.getLocation().getPathDistance(nearestPill) < pacman.getLocation().getPathDistance(nearestPill))
+							actions[i] = defender.getNextDir(attackPosition, true);
+							//if she isn't close, then the attacker will go right for her. Kind of like a guard dog.
+
+						else
+							actions[i] = defender.getNextDir(nearestPill, true);
 					}
 			}
 		}
